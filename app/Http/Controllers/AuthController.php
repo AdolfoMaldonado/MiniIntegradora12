64d26bfac406e0c575312e9b42fa524b65c34b72
AdolfoMaldonado <cesara.maldonaod@softtek.com>
2023-08-16 00:42:23 -0600

    Cambio 15/08/23 12:42am

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        $user = User::create([
+            'name' => $validatedData['name'],
+            'email' => $validatedData['email'],
+            'password' => Hash::make($validatedData['password']),
+        ]);
+
+        return response()->json(['message' => 'User registered successfully'], 201);
+    }
+
+    public function login(Request $request)
+    {
+        $validatedData = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (!Auth::attempt($validatedData)) {
+            return response()->json(['error' => 'Invalid credentials'], 401);
+        }
+
+        $user = Auth::user();
+        $token = $user->createToken('Access Token')->plainTextToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,8 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Register;
 
 class RegisterController extends Controller
 {
-    //
+    public function getAdafruitDataAndStore(Request $request)
+    {
+        $url = 'https://io.adafruit.com/api/v2/YOUR_USERNAME/feeds/YOUR_FEED/data';
+        
+        $response = Http::withHeaders([
+            'X-AIO-Key' => 'YOUR_AIO_KEY',
+        ])->get($url);
+
+        $data = $response->json();
+
+        foreach ($data as $entry) {
+            // Crea un nuevo registro en la tabla 'registers'
+            Register::create([
+                'datos' => $entry['value'], // Ajusta esto según la estructura de tus datos
+                'unidades' => 'Tu Unidad', // Ajusta las unidades según tus necesidades
+                'sensor_id' => 1, // Si deseas asociar el registro a un sensor específico
+                'equipo_id' => 1, // Si deseas asociar el registro a un equipo específico
+            ]);
+        }
+
+        return redirect()->route('home')->with('success', 'Datos de Adafruit almacenados correctamente.');
+    }
 }
