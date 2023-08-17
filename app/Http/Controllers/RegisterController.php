25dafdba60ad1e0e39dad017c6bb8c6ebe9bbdf9
AdolfoMaldonado <cesara.maldonaod@softtek.com>
2023-08-17 01:48:45 -0600

    esta madre ya se trae datos

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -8,22 +8,53 @@ use App\Models\Register;
 
 class RegisterController extends Controller
 {
-    public function datos(Request $request){
-        $token = $request->header('X-AIO-key');
+    private function fetchDataFromFeed($token, $feedName)
+    {
         $response = Http::withHeaders(['X-AIO-key' => $token])
-            ->get('https://io.adafruit.com/api/v2/fermurilllo/feeds/distancia/data');
-        $datos = $response->json();
+            ->get("https://io.adafruit.com/api/v2/AlbMaldonado2994/feeds/{$feedName}");
+        return $response->json();
+    }
+
+    public function obtenerDatosFeed(Request $request, $feedName)
+    {
+        $token = $request->header('X-AIO-key');
+        $feedData = $this->fetchDataFromFeed($token, $feedName . '/data');
+    
         return response()->json([
             'status' => 'ok',
-            'datos' => $datos
+            'datos' => $feedData
         ], 200);
     }
-    public function ultimoDato(Request $request){
+    
+
+    public function obtenerUltimoDato(Request $request, $feedName)
+    {
         $token = $request->header('X-AIO-key');
-        $response = Http::withHeaders(['X-AIO-key' => $token])
-            ->get('https://io.adafruit.com/api/v2/AlbMaldonado2994/feeds/temperatura');
-        $datos = $response->json();
+        $datos = $this->fetchDataFromFeed($token, $feedName);
+    
         return response()->json($datos, 200);
     }
+    
+
+    public function obtenerTodo(Request $request)
+    {
+        $token = $request->header('X-AIO-key');
+
+        $feeds = ['humedad', 'lluvia', 'sonido','temperatura'];
+
+        $dataSubset = [];
+
+        foreach ($feeds as $feed) {
+            $feedData = $this->fetchDataFromFeed($token, $feed);
+            $dataSubset[$feed] = [
+                'name' => $feedData['name'],
+                'last_value' => $feedData['last_value'],
+                'created_at' => $feedData['created_at']
+            ];
+        }
+
+        return response()->json($dataSubset, 200);
+    }
+
 
 }
--- routes/api.php
+++ routes/api.php
@@ -4,25 +4,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\RegisterController;
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::group(['middleware' => 'auth:sanctum'], function () {
 
-    return $request->user();
-    Route::get('/datos', [RegisterController::class, 'datos']);
-    Route::get('/ultimoDato', [RegisterController::class, 'ultimoDato']);
-
+    Route::get('/datos/{feedName}', [RegisterController::class, 'obtenerDatosFeed']);
+    Route::get('/ultimoDato/{feedName}', [RegisterController::class, 'obtenerUltimoDato']);
+    Route::get('/obtenerTodo', [RegisterController::class, 'obtenerTodo']);
 });
